961958a4b1fcff9de3dc143e290029cfdad78ef3
slyeet03 <shobhit7419@gmail.com>
2026-04-02 23:06:45 +0530

    modified structure

---
+++ server.js
@@ -0,0 +1,37 @@
+const cors = require("cors");
+const express = require("express");
+const session = require("express-session");
+
+const app = express();
+
+app.use(
+  cors({
+    origin: ["https://localhost:3000", "https://127.0.0.1:3000"],
+  }),
+);
+
+//middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public")); //use css
+app.set("view engine", "ejs");
+
+//session setup
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false, //save only when smthing is modified
+    saveUninitialized: false,
+  }),
+);
+
+//routes
+const homeRoutes = require("./routes/home");
+app.use("/", homeRoutes);
+const worksRoutes = require("./routes/works");
+app.use("/works", worksRoutes);
+
+//start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Server running on port ${PORT}");
+});
